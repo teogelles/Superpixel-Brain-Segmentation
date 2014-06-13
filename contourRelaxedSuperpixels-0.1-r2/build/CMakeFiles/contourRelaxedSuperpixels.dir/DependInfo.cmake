cc4e5fb77c5da72300c5f4b3d1dda5c6508e9bd9
Teo Gelles <teogelles@gmail.com>
2014-06-13 15:45:57 -0400

    Added contour superpixels

---
+++ contourRelaxedSuperpixels-0.1-r2/build/CMakeFiles/contourRelaxedSuperpixels.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tgelles1/Desktop/contourRelaxedSuperpixels-0.1-r2/src/contourRelaxedSuperpixels.cpp" "/home/tgelles1/Desktop/contourRelaxedSuperpixels-0.1-r2/build/CMakeFiles/contourRelaxedSuperpixels.dir/contourRelaxedSuperpixels.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ contourRelaxedSuperpixels-0.1-r2/build/CMakeFiles/contourRelaxedSuperpixels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/contourRelaxedSuperpixels.dir/contourRelaxedSuperpixels.cpp.o"
+  "contourRelaxedSuperpixels.pdb"
+  "contourRelaxedSuperpixels"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/contourRelaxedSuperpixels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
