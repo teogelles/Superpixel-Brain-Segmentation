29a88523ebcab42d86aaeec5ddb919dc7bffd5e4
Teo Gelles <teogelles@gmail.com>
2014-06-17 16:40:33 -0400

    Fixed directory issues

--- scripts/batchCOREG_job.m
+++
@@ -1,9 +0,0 @@
-%-----------------------------------------------------------------------
-% Job configuration created by cfg_util (rev $Rev: 4252 $)
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.spatial.coreg.write.ref = {'/acmi/fmri/AD_T1/patient2.nii,1'};
-matlabbatch{1}.spm.spatial.coreg.write.source = {'/acmi/fmri/altAtlas/atlas116.img,1'};
-matlabbatch{1}.spm.spatial.coreg.write.roptions.interp = 1;
-matlabbatch{1}.spm.spatial.coreg.write.roptions.wrap = [0 0 0];
-matlabbatch{1}.spm.spatial.coreg.write.roptions.mask = 0;
-matlabbatch{1}.spm.spatial.coreg.write.roptions.prefix = '1';
