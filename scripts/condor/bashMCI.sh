5ff248b98704ae09f8e128bd1f52cd0cba805d5e
Andrew Gilchrist-Scott <agilchr1@cs.swarthmore.edu>
2014-07-07 09:46:37 -0400

    Removing condor files from version control

--- scripts/condor/bashMCI.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-foo=200
-bar=$1
-((foo+=bar))
-matlab -nojvm -nosplash -r "condor_runSLIC($1 + 1, 'MCI'); exit"
--- scripts/condor/bashSLICnumIters.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-foo=200
-bar=$1
-((foo+=bar))
-matlab -nojvm -nosplash -r "condor_runSLIC(1,'IBSR',1,500,20,4 + ($1)); exit"
