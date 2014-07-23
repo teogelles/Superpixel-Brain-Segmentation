443d73c1fa5378cfaba8987b048ad8932c7aec8a
Andrew Gilchrist-Scott <agilchr1@cs.swarthmore.edu>
2014-07-23 14:06:25 -0400

    Changes to KFacts algo

--- scripts/testKFact.m
+++ scripts/testKFact.m
@@ -2,22 +2,48 @@
 % determining k factors of n which have the minimum spread between
 % the min and the max factors
 
-function testKFact(iStart,iEnd)
+function testKFact(iStart,iEnd,kStart,kEnd)
     
     if ~exist('iEnd','var')
         iStart = 100;
         iEnd = 1000;
     end
     
-    types = {'minMin','minMed','minMax'};
-    scores = [0, 0, 0];
+    if ~exist('kEnd','var')
+        kStart = 3;
+        kEnd = 8;
+    end
+    
+    types = {'minMin','minMed','minMax','buckets'};
+    scores = [0, 0, 0, 0];
+    times = [0, 0, 0, 0];
 
     for i = iStart:iEnd
-        fprintf('Num: %d\n',i);
-        for j = 3:8
+        %fprintf('Num: %d\n',i);
+        for j = 3:11
+            tic;
             a = minMin(i,j);
+            atime = toc;
+            tic;
             b = minMed(i,j);
+            btime = toc;
+            tic;
             c = minMax(i,j);
+            ctime = toc;
+            tic;
+            d = buckets(i,j);
+            dtime = toc;
+            
+            times = times + [atime btime ctime btime];
+            
+            if (i == 504 || i == 336) && (j == 3);
+                fprintf('For %d broken into %d:\n',i,j);
+                disp(a);
+                disp(b);
+                disp(c);
+                disp(d);
+                fprintf('\n');
+            end
             
             %check if algos worked at all
             if (prod(a) ~= i) || (length(a) ~= j)
@@ -32,22 +58,33 @@ function testKFact(iStart,iEnd)
                 fprintf(['Problem with c; %d not %d prod, %d not %d ' ...
                          'factors\n'],prod(c),i,length(c),j);
             end 
+            if (prod(d) ~= i) || (length(d) ~= j)
+                fprintf(['Problem with d; %d not %d prod, %d not %d ' ...
+                         'factors\n'],prod(d),i,length(d),j);
+            end 
             
-            spreadA = max(a) - min(a);
-            spreadB = max(b) - min(b);
-            spreadC = max(a) - min(a);
+            spreadA = a(end) - a(1);
+            spreadB = b(end) - b(1);
+            spreadC = c(end) - c(1);
+            spreadD = d(end) - d(1);
+            spreads = [spreadA spreadB spreadC spreadD];
             
-            if (spreadA ~= spreadC) || (spreadB ~= spreadC) || ...
-                    (spreadA ~= spreadB)
-                [~, ind] = min([spreadA spreadB spreadC]);
+            if ~all(spreads == spreads(1))
+                [~, ind] = min(spreads);
                 type = types{ind};
-                fprintf('Winner: %s for %d broken in %d\n',type,i, j);
+                %fprintf('Winner: %s for %d broken in %d\n',type,i, j);
                 scores(ind) = scores(ind) + 1;
             end
         end
     end
     
-    scores
+    times = times/(i*j);
+    
+    fprintf('Here are the scores:\n');
+    for i = 1:length(scores)
+        fprintf('%s : %d, avg time %.10f\n',types{i},scores(i),times(i));
+    end
+    
 end
 
 function k_facts = minMin(n,k)
@@ -88,10 +125,11 @@ function k_facts = minMax(n,k)
     end
     
     k_facts = [];
-    while (length(k_facts) < k) && (length(facts) > 1)
+    while (length(k_facts) < k) && (length(facts) > 1) && ...
+            (length([facts k_facts]) ~= k)
         newFact = facts(1)*facts(end);
         k_facts(end+1) = newFact;
-        fact = facts(2:end-1);
+        facts = facts(2:end-1);
     end
     
     if length([k_facts facts]) == k
@@ -109,14 +147,12 @@ function k_facts = minMax(n,k)
         k_facts(end+1) = newFact;
         k_facts = sort(k_facts);
     end
-    
-    k_facts = fact;
 end
 
 function k_facts = minMed(n,k)
     
     facts = factor(n);
-    
+     
     if length(facts) == k
         k_facts = facts;
         return
@@ -128,7 +164,8 @@ function k_facts = minMed(n,k)
     end
         
     k_facts = [];
-    while (length(k_facts) < k) && (length(facts) > 2)
+    while (length(k_facts) < k) && (length(facts) > 2) && ...
+            (length([facts k_facts]) ~= k)
         newFact = facts(1)*facts(ceil(end/2));
         k_facts(end + 1) = newFact;
         facts = [facts(2:ceil(end/2)-1) facts(ceil(end/2)+1:end)];
@@ -151,3 +188,54 @@ function k_facts = minMed(n,k)
     end
     
 end
+
+function bucks = buckets(n,k)
+    facts = factor(n);
+    
+    if length(facts) == k
+        bucks = facts;
+        return
+    elseif length(facts) < k
+        bucks = zeros(k,1);
+        bucks(1:(k-length(facts))) = 1;
+        bucks((k-length(facts)+1):end) = facts;
+        return
+    end
+    
+    bucks = ones(1,k);
+    ideal = n^(1/k);
+    
+    randomize = false;
+    if randomize
+        randfactsind = randperm(length(facts));
+        facts = facts(randfactsind);
+    end
+    
+    reverse = false;
+    if reverse
+        facts = sort(facts,'descend');
+    end
+    
+    for fact = facts
+        err = zeros(size(bucks));
+        for i = 1:length(bucks)
+            err(i) = abs(ideal - bucks(i)*fact);
+        end
+        [~, minI] = min(err);
+        bucks(minI) = bucks(minI)*fact;
+    end
+        
+    % for fact = facts
+    %     testVar = zeros(k,1);
+    %     for i = 1:length(bucks)
+    %         bucks(i) = bucks(i)*fact;
+    %         testVar(i) = var(bucks);
+    %         bucks(i) = bucks(i)/fact;
+    %     end
+    %     [~, minI] = min(testVar);
+    %     bucks(minI) = bucks(minI)*fact;
+    % end
+        
+    bucks = sort(bucks);
+end
+        
